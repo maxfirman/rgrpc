49fd10d844a3979a1c9f9c07fb36b845f739b6a1
Max Firman <max.firman@gmail.com>
2021-02-17 14:15:15 +0000

    Run stream in Dockerfile

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 ./rgrpc/server/server.out &
-R -e 'library("rgrpc");rgrpc::get_data("hello")'
+R -e 'library("rgrpc");rgrpc::get_data("hello");rgrpc::get_data_stream("hello stream", 1000000)'
